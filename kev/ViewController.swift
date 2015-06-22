f57d737dc8c30308787e2175c3f8c0f230fcf490
PJ Akuhata <peter.akuhata@gmail.com>
2015-06-22 21:49:57 +1200

    Initial Commit

---
+++ kev/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  kev
+//
+//  Created by Peter Akuhata on 22/06/15.
+//  Copyright (c) 2015 Peter Akuhata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
