6b9c510a9ffc1d143805c5772af80a8373283e13
PJ Akuhata <peter.akuhata@gmail.com>
2015-06-22 22:23:19 +1200

    playing with core animator

--- kev/ViewController.swift
+++ kev/ViewController.swift
@@ -9,17 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    
+    @IBOutlet weak var kevView: KevView!
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func didTouchWalkButton(sender: UIButton) {
+        self.kevView.addWalkingAnimation()
+    }
 
+    @IBAction func didTouchFadeInButton(sender: UIButton) {
+        self.kevView.addFadeInAnimation()
+    }
 }
 
